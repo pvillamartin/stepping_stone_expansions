5827ca5eb43627bc72fa6a36790de37bf1689299
btweinstein <btweinstein@gmail.com>
2014-04-28 16:03:08 -0400

    Change before I revert.

--- setup.py
+++ setup.py
@@ -3,14 +3,8 @@ from distutils.extension import Extension
 from Cython.Build import cythonize
 
 extensions = [
-    Extension("range_expansions", sources=["range_expansions.pyx"],
-              extra_compile_args=['-O2']),
-    Extension("random_cython", sources=["random_cython.cpp"],
-              language="c++",
-              extra_compile_args=['-std=c++11', '-O2']),
-        Extension("random_test", sources=["random_test.pyx"],
-              language="c++",
-              extra_compile_args=['-std=c++11', '-O2'])
+    Extension("src/range_expansions", sources=["range_expansions.pyx"],
+              extra_compile_args=['-O2'])
 ]
 
 setup(
