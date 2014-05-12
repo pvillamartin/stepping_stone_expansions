672fd712fb1ab52bb4da51dc3b79fa4d7b2a8602
btweinstein <btweinstein@gmail.com>
2014-05-12 14:41:27 -0400

    Making lattices of demes now.

--- src/range_expansions.pyx
+++ src/range_expansions.pyx
@@ -22,21 +22,22 @@ cdef class Deme:
     cdef readonly long num_alleles
     cdef readonly long[:] binned_alleles
     cdef readonly long num_members
+    cdef readonly long m_swap
     cdef py_uniform_random r
 
-    def __init__(Deme self,  long num_alleles, Individual[:] members):
+    def __init__(Deme self,  long num_alleles, Individual[:] members,int m_swap=0):
         self.members = members
         self.num_members = len(members)
         self.num_alleles = num_alleles
         self.binned_alleles = self.bin_alleles()
+        self.m_swap = m_swap
 
         cdef double seed
-
         seed = random.randint(0, sys.maxint)
 
         self.r = py_uniform_random(0, self.num_members - 1, seed)
 
-    cdef reproduce(Deme self):
+    cpdef reproduce(Deme self):
         cdef int to_reproduce
         cdef int to_die
 
@@ -51,18 +52,58 @@ cdef class Deme:
         cdef Individual reproducer = self.members[to_reproduce]
         self.members[to_die] = reproducer
 
+    cpdef swap_members(Deme self, Deme other):
+        cdef int i
+
+        cdef int self_swap_index
+        cdef int other_swap_index
+        cdef Individual self_swap
+        cdef Individual other_swap
+
+        self_swap_index = self.r.get_random()
+        other_swap_index = other.r.get_random()
+
+        self_swap = self.members[self_swap_index]
+        other_swap = other.members[other_swap_index]
+
+        ## Update allele array of BOTH demes
+        self.binned_alleles[self_swap.allele_id] -= 1
+        self.binned_alleles[other_swap.allele_id] += 1
+
+        other.binned_alleles[other_swap.allele_id] -= 1
+        other.binned_alleles[self_swap.allele_id] += 1
+
+        ## Update members
+        self.members[self_swap_index] = other_swap
+        other.members[other_swap_index] = self_swap
+
     cpdef get_alleles(Deme self):
         return [individual.allele_id for individual in self.members]
 
     cdef bin_alleles(Deme self):
         return np.bincount(self.get_alleles(), minlength=self.num_alleles)
 
-def simulate_deme(Deme deme, long numAlleles, long numIterations=100):
-    cdef long[:,:] history
+    cpdef check_allele_frequency(Deme self):
+        '''A diagnostic test that makes sure that the result returned by
+        bin_alleles is the same as the current allele frequency. If it is false,
+        there is a problem in the code somewhere.'''
 
-    history = np.empty((numIterations, numAlleles), dtype=np.long)
+        return np.array_equal(self.binned_alleles, self.bin_alleles())
+
+def simulate_deme(Deme deme, long num_generations=100):
+    cdef long[:,:] history
+    cdef double[:] fractional_generation
+    cdef long num_iterations
+
+    num_iterations = num_generations * deme.num_members
+
+    fractional_generation = np.empty(num_iterations, dtype=np.float)
+    history = np.empty((num_iterations, deme.num_alleles), dtype=np.long)
+
     cdef long i
-    for i in range(numIterations):
+    for i in range(num_iterations):
+        fractional_generation[i] = float(i)/deme.num_members
         history[i, :] = deme.binned_alleles
         deme.reproduce()
-    return history
+
+    return fractional_generation, history
