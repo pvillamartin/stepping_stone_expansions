b7e660a8494a12acced90800e825c0adb0f3b5bc
bryan <btweinstein@gmail.com>
2014-11-06 14:23:18 -0500

    Fixed setup so anaconda can successfully find numpy headers.

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
 import cython_gsl
+import numpy as np
 
 extensions = [
     Extension("range_expansions", sources=["src/range_expansions.pyx"], language="c++",
               libraries = cython_gsl.get_libraries(),
               library_dirs = [cython_gsl.get_library_dir()],
-              include_dirs = [cython_gsl.get_cython_include_dir()])
+              include_dirs = [cython_gsl.get_cython_include_dir(), np.get_include()])
 ]
 
 setup(
@@ -15,7 +16,7 @@ setup(
     version='0.5',
     author='Bryan T. Weinstein',
     author_email = 'bweinstein@seas.harvard.edu',
-    include_dirs = [cython_gsl.get_include()],
+    include_dirs = [cython_gsl.get_include(), np.get_include()],
     ext_modules = cythonize(extensions, annotate=True),
     py_modules = ['range_expansion_convenience']
 )
