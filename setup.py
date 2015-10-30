fdc5cf02a9e4d68ece01ce3ef95aed26db9042fd
bryan <btweinstein@gmail.com>
2015-10-30 17:22:35 -0400

    Improving my package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import cython_gsl
 import numpy as np
 
 extensions = [
-    Extension("range_expansions", sources=["src/range_expansions.pyx"], language="c++",
+    Extension("stepping_stone.range_expansion", sources=["stepping_stone/range_expansion.pyx"], language='c',
               libraries = cython_gsl.get_libraries(),
               library_dirs = [cython_gsl.get_library_dir()],
               include_dirs = [cython_gsl.get_cython_include_dir(), np.get_include()])
@@ -13,10 +13,10 @@ extensions = [
 
 setup(
     name="Range Expansions",
-    version='0.5',
+    version='0.6',
     author='Bryan T. Weinstein',
     author_email = 'bweinstein@seas.harvard.edu',
     include_dirs = [cython_gsl.get_include(), np.get_include()],
+    packages=['stepping_stone'],
     ext_modules = cythonize(extensions, annotate=True),
-    py_modules = ['range_expansion_convenience']
 )
--- test/spaghetti.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'bryan'
-
-from random_cython_port import py_uniform_random
-import numpy as np
-
-muffin = np.random.rand()*10**6
-
-derp = py_uniform_random(0, 100, muffin)
-print derp.get_random()
